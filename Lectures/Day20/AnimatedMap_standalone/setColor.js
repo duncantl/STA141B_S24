df7a7aafad5d642bb7e93c66e67b9c783b49d09b
Duncan Temple Lang <duncan@r-project.org>
2024-06-10 11:27:57 -0700

    inline the SVG documents so there is no cross-origin issue and can view this w/o a web server.  Also simplified the setStatesColorEmbedded JS function as don't need XPath to the svg:path nodes.

---
+++ Lectures/Day20/AnimatedMap_standalone/setColor.js
@@ -0,0 +1,75 @@
+// better version that handles states with multiple pologyons
+function setStatesColorEmbedded(n)
+{
+    for(var k in stateColors) {
+	console.log(k);
+        var p = document.getElementById(k);
+	if(p)
+	    p.style.fill = stateColors[k][n];
+    }
+
+
+    var d = document.getElementById("date");
+    d.innerHTML = dateLabels[n]; // format as a date
+
+      // set the current value of the slider to show where the current data in the animation.
+    d = document.getElementById("setDate");
+    d.value = n;
+    
+    return(true);
+}
+
+
+function svgNSResolver(prefix) { 
+     if (prefix == 'svg') 
+         return 'http://www.w3.org/2000/svg';
+     return null;
+}
+
+
+
+
+function setStatesColorInline(n)
+{
+//    var svg = document.embeds[0].getSVGDocument();
+    for(var k in stateColors) {
+/*
+	var xp = "//svg:path[ @id = '" + k + "']";
+	var i = svg.evaluate(xp, svg, svgNSResolver, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE );
+	var p = i.snapshotItem(0);
+*/
+	var p = document.getElementById(k);
+//	alert(k + " -> "  + p);
+	if(p)
+	    p.style.fill = stateColors[k][n];// setAttribute("fill", stateColors[k][n]);
+    }
+
+    return(true);
+}
+
+
+
+function old_setStatesColorEmbedded(n)
+{
+    var svg = document.getElementById("svgMap").getSVGDocument();
+    //embeds[0].getSVGDocument();
+    for(var k in stateColors) {
+	    /*	
+	var xp = "//svg:path[ @id = '" + k + "']";
+	var i = svg.evaluate(xp, svg, svgNSResolver, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE );
+	var p = i.snapshotItem(0);
+*/
+	var p = svg.getElementById(k);
+	if(p)
+	    p.style.fill = stateColors[k][n];// setAttribute("fill", stateColors[k][n]);
+	else if(n == 0)
+	    console.log("no element for " + k);
+    }
+
+    var d = document.getElementById("date");
+    d.innerHTML = dateLabels[n]; // format as a date
+
+    return(true);
+}
+
+
